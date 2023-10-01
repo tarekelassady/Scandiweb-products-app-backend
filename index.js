83cf518ade3597833ec9e8f414c653334707b8e0
Tarek Elassady <tarekelassady@gmail.com>
2023-10-01 04:39:34 +0200

    sixth commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import dotenv from "dotenv"
 
 const app= express();
 dotenv.config();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use("/api/products",productsController);
 
