8c3b92d58918afc6064d52a681e841b0a78bdcc7
Tarek Elassady <tarekelassady@gmail.com>
2023-10-01 19:59:13 +0200

    32th commit

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-import mysql from 'mysql'
+import mysql from 'mysql2'
 
 // const urlDB=`mysql://root:Mx2Tb7YFP2ndln3Foglp@containers-us-west-191.railway.app:7795/railway`
 const urlDB=`mysql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.DATABASE}`
