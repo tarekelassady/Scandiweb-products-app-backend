92d821b062de53a27c8344e6cf120ccbdeb8a08b
Tarek Elassady <tarekelassady@gmail.com>
2023-10-01 04:48:23 +0200

    9th commit

--- index.js
+++ index.js
@@ -10,29 +10,17 @@ import dotenv from "dotenv"
 const app= express();
 dotenv.config();
 // app.use(cors());
-app.use(express.json());
+
+app.get('/' , (req,res) => {
+  res.send('hey Tarek!')
+})
+
+// app.use(express.json());
 app.use("/api/products",productsController);
 
 app.get('/' , (req,res) => {
   res.send('hey Tarek!')
 })
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//       cb(null, '../client/public/upload')
-//     },
-//     filename: (req, file, cb) => {
-//       cb(null, Date.now()+file.originalname);
-//     }
-//   })
-// const upload=multer({storage});
-// app.post('/api/upload',upload.single('file'), (req,res)=>{
-//     const file=req.file;
-//     res.status(200).json(file.filename);
-// })
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// app.use(express.static(path.join(__dirname+"/public")));
 
 
 
